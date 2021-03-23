bf1b7c067eaedf53564dc5cb4ae2233cd202db71
Guilherme Illescas <gui.illescas@gmail.com>
2021-03-23 13:38:19 -0300

    Fix identation and add key value on map method

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -33,9 +33,9 @@ const Home = (): JSX.Element => {
   useEffect(() => {
     async function loadProducts() {
       api.get('/products')
-      .then(response => {
-        setProducts(response.data)
-      })
+        .then(response => {
+          setProducts(response.data)
+        })
     }
 
     loadProducts();
@@ -48,23 +48,23 @@ const Home = (): JSX.Element => {
   return (
     <ProductList>
       {products.map(product => (
-        <li>
-        <img src={product.image} alt={`Foto de ${product.title}`} />
-        <strong>{product.title}</strong>
-        <span>{formatPrice(product.price)}</span>
-        <button
-          type="button"
-          data-testid="add-product-button"
-          onClick={() => handleAddProduct(product.id)}
-        >
-          <div data-testid="cart-product-quantity">
-            <MdAddShoppingCart size={16} color="#FFF" />
-            {cartItemsAmount[product.id] || 0}
-          </div>
+        <li key={product.id}>
+          <img src={product.image} alt={`Foto de ${product.title}`} />
+          <strong>{product.title}</strong>
+          <span>{formatPrice(product.price)}</span>
+          <button
+            type="button"
+            data-testid="add-product-button"
+            onClick={() => handleAddProduct(product.id)}
+          >
+            <div data-testid="cart-product-quantity">
+              <MdAddShoppingCart size={16} color="#FFF" />
+              {cartItemsAmount[product.id] || 0}
+            </div>
 
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
+            <span>ADICIONAR AO CARRINHO</span>
+          </button>
+        </li>
       ))}
     </ProductList>
   );
